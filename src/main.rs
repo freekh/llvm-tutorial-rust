1e26952626583c66c206f4ff0ffd783c3f6222b2
Fredrik Ekholdt <freekh@gmail.com>
2018-01-24 15:27:54 +0100

    Do you remember

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::io::*;
+
+enum Token {
+  Eof,
+  Number(f64),
+  Identifier(String),
+}
+
+fn getToken() {
+
+}
+
 fn main() {
-  println!("Hello");
+  let reader = BufReader::new(stdin());
+  let buf: []
+  while (reader.read(buf).is_ok()) {
+    getToken();
+  }
 }
